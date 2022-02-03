423d1558baed492e08dafc743089eb472d20a09f
unknown <you@example.com>
2022-02-03 12:24:49 +0500

    keraksiz kod ochirildi

---
+++ script.js
@@ -0,0 +1,11 @@
+
+var num = 2004
+console.log(2022-num);
+
+
+let speed = 10;
+if(speed<=120 &&  speed>=90 ) { console.log("Jarima to'lansin");
+}else if (speed<=89 && speed>=60) {console.log("Normal tezlik");   
+}else if (speed<=59 && speed>=40) {console.log("Siz sekin harakatlanyapsiz");  
+} else console.log("Yurmadingiz"); 
+
